67b8526a1b55877402ff1b425083c7af54516834
santosmita sahoo <santosmitasahoo57@gmail.com>
2026-09-26 11:57:51 +0530

    Add GFG solution: Check If Circular Linked List | Practice

---
+++ GeeksForGeeks/LinkedList/check-if-circular-linked-list-practice.java
@@ -0,0 +1,15 @@
+class Solution {
+    boolean isCircular(Node head) {
+
+        if (head == null)
+            return true;
+
+        Node temp = head.next;
+
+        while (temp != null && temp != head) {
+            temp = temp.next;
+        }
+
+        return temp == head;
+    }
+}
