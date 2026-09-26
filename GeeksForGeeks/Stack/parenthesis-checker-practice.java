0aedf5d5c90172549d39fde7a14c05e15c6f035a
santosmita sahoo <santosmitasahoo57@gmail.com>
2026-09-26 11:00:25 +0530

    Add GFG solution: Parenthesis Checker | Practice

---
+++ GeeksForGeeks/Stack/parenthesis-checker-practice.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } 
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
