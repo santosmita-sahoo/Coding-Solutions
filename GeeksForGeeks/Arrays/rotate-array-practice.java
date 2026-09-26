ea3352e9592951c20db2cfdd6bb9ec7b304acaf3
santosmita sahoo <santosmitasahoo57@gmail.com>
2026-09-26 10:55:54 +0530

    Add GFG solution: Rotate Array | Practice

---
+++ GeeksForGeeks/Arrays/rotate-array-practice.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotateArr(int[] arr, int d) {
+        int n = arr.length;
+
+        d = d % n;
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+    private void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
